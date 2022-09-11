5013ab043b1461f502f686aa3ccf62e06c87a9f9
Silver418 <87504286+Silver418@users.noreply.github.com>
2022-09-11 07:37:34 -0600

    Transactions By Date - primary stinters removed
    Seasons' Transactions by Date view will exclude players who have a Primary Stint listed, as they are intended to be treated as with that team for the whole season.

--- BaseballModel/Models/TransactionModels/TransactionList.cs
+++ BaseballModel/Models/TransactionModels/TransactionList.cs
@@ -10,53 +10,65 @@ namespace BaseballModel.Models {
 
         public TransactionList(SeasonPersonStint sps) {
             foreach (PersonStint player in sps.GetPlayers()) {
-                if (player.IsComplete) {    //don't want incomplete records
-                                            //may assume from here that StintStart & StintEnd are filled for all this player's stints
+                if (player.IsComplete) {    //want complete records only
+                                            //can assume from here that StintStart & StintEnd are filled for all this player's stints
+                    
+                    //do not include players who have a primary stint listed
+                    bool primaryStintFound = false;
                     foreach (StintRecord stint in player.GetStints()) {
-                        if (!stint.IgnoreStint) {
-                            DateTime stintStart = (DateTime)stint.StintStart;
-                            DateTime stintEnd = (DateTime)stint.StintEnd;
-                            
-                            //copy stint's start date & team to transactions
-                            if (stintStart.Date != sps.Season.SeasonStart.Date) { //do not want stints beginning on season's first day
-                                //check for matching record in transactions list
-                                TransactionRecord? matching = (from record in transactions
-                                                               where record.PlayerId == stint.PlayerId
-                                                               && record.TransactionDate.Date == stintStart.Date
-                                                               select record).FirstOrDefault();
-                                //if match, copy team player is joining
-                                if (matching != null) {
-                                    matching.JoinTeam = stint.TeamName;
-                                }
-                                //if no match, create new record & add to transactions
-                                else {
-                                    TransactionRecord record = new TransactionRecord(player.PlayerId, player.NameFirst, player.NameLast, stintStart);
-                                    record.JoinTeam = stint.TeamName;
-                                    transactions.Add(record);
-                                }
-                            }
-
-                            //copy stint's end date & team to transactions
-                            if (stintEnd.Date != sps.Season.SeasonEnd.Date) { //do not want stints ending on season's last day
-                                //check for matching record in transactions list
-                                TransactionRecord? matching = (from record in transactions
-                                                               where record.PlayerId == stint.PlayerId
-                                                               && record.TransactionDate.Date == stintEnd.Date
-                                                               select record).FirstOrDefault();
-                                //if match, copy team player is leaving
-                                if (matching != null) {
-                                    matching.LeaveTeam = stint.TeamName;
-                                }
-                                //if no match, create new record & add to transactions
-                                else {
-                                    TransactionRecord record = new(player.PlayerId, player.NameFirst, player.NameLast, stintEnd);
-                                    record.LeaveTeam = stint.TeamName;
-                                    transactions.Add(record);
-                                }
-                            }
+                        if (stint.PrimaryStint) {
+                            primaryStintFound = true;
+                            break;
                         }
+                    }
+                    if (!primaryStintFound) {
+                        foreach (StintRecord stint in player.GetStints()) {
+                            if (!stint.IgnoreStint) {
+                                DateTime stintStart = (DateTime)stint.StintStart;
+                                DateTime stintEnd = (DateTime)stint.StintEnd;
 
-                    }//end foreach stint
+                                //copy stint's start date & team to transactions
+                                if (stintStart.Date != sps.Season.SeasonStart.Date) { //do not want stints beginning on season's first day
+                                                                                      //check for matching record in transactions list
+                                    TransactionRecord? matching = (from record in transactions
+                                                                   where record.PlayerId == stint.PlayerId
+                                                                   && record.TransactionDate.Date == stintStart.Date
+                                                                   select record).FirstOrDefault();
+                                    //if match, copy team player is joining
+                                    if (matching != null) {
+                                        matching.JoinTeam = stint.TeamName;
+                                    }
+                                    //if no match, create new record & add to transactions
+                                    else {
+                                        TransactionRecord record = new TransactionRecord(player.PlayerId, player.NameFirst, player.NameLast, stintStart);
+                                        record.JoinTeam = stint.TeamName;
+                                        transactions.Add(record);
+                                    }
+                                }
+
+                                //copy stint's end date & team to transactions
+                                if (stintEnd.Date != sps.Season.SeasonEnd.Date) { //do not want stints ending on season's last day
+                                                                                  //check for matching record in transactions list
+                                    TransactionRecord? matching = (from record in transactions
+                                                                   where record.PlayerId == stint.PlayerId
+                                                                   && record.TransactionDate.Date == stintEnd.Date
+                                                                   select record).FirstOrDefault();
+                                    //if match, copy team player is leaving
+                                    if (matching != null) {
+                                        matching.LeaveTeam = stint.TeamName;
+                                    }
+                                    //if no match, create new record & add to transactions
+                                    else {
+                                        TransactionRecord record = new(player.PlayerId, player.NameFirst, player.NameLast, stintEnd);
+                                        record.LeaveTeam = stint.TeamName;
+                                        transactions.Add(record);
+                                    }
+                                }
+                            }
+
+                        }//end foreach stint
+                    }
+
                 } //end if player's stints IsComplete
             } //end foreach player
 
