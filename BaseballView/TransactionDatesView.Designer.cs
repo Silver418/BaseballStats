7f2f1361005c3154892b8ad2935dbddce624b180
Silver418 <87504286+Silver418@users.noreply.github.com>
2022-09-11 07:38:39 -0600

    transactions by date - labeling change
    Updated labels in GUI to reflect exclusion of players with primary stints

--- BaseballView/TransactionDatesView.Designer.cs
+++ BaseballView/TransactionDatesView.Designer.cs
@@ -113,9 +113,9 @@
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.Location = new System.Drawing.Point(0, 21);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(301, 15);
+            this.label2.Size = new System.Drawing.Size(420, 15);
             this.label2.TabIndex = 16;
-            this.label2.Text = "Only players with completed stint records are shown.";
+            this.label2.Text = "Only players with completed stint records and no primary stints are shown.";
             // 
             // TransactionDatesView
             // 
